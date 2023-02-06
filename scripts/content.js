de6815f5c1c0effa3611cab7b1aa181631e32181
Shane McQuerter <smcquerter@trumidtech.com>
2023-02-05 22:03:30 -0800

    attempt to add buttons on dom mutation

--- scripts/content.js
+++ scripts/content.js
@@ -1,45 +1,117 @@
-const jiraIssueHeader = document.querySelector("#jira-issue-header");
-const actionsContainer = jiraIssueHeader.firstChild.firstChild.firstChild;
-const ticketId = document.querySelector(
-  'a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"] > span'
-).textContent;
+const CLIPBOARD_ICON = "&#128203";
+const CHECKMARK_ICON = "&#10004;";
 
-actionsContainer.style.flexGrow = "2";
+function createCopyButton(
+  { buttonText, copyText, width },
+  existingButtonCssText
+) {
+  const newButton = document.createElement("button");
+  const textWithClipboard = `${CLIPBOARD_ICON} ${buttonText}`;
 
-const copyTicketButton = document.createElement("button");
-copyTicketButton.textContent = ticketId;
+  newButton.innerHTML = textWithClipboard;
+  newButton.dataset.copyText = copyText;
+  newButton.title = `Copy: ${copyText}`;
+  // assign existing button styles to new copy button
+  newButton.style.cssText = existingButtonCssText;
+  // explicitly set button width to accommodate different icon widths
+  newButton.style.width = width || "auto";
 
-actionsContainer.insertAdjacentElement("afterEnd", copyTicketButton);
+  // temporarily show success icon after successful copy click
+  newButton.addEventListener("click", async function () {
+    try {
+      await navigator.clipboard.writeText(copyText);
 
-copyTicketButton.addEventListener("click", function () {
-  console.log("Adding");
+      newButton.innerHTML = `${CHECKMARK_ICON} ${buttonText}`;
 
-  navigator.clipboard.writeText(copyTicketButton.textContent).then(
-    function () {
-      console.log("Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Could not copy text: ", err);
+      setTimeout(() => {
+        newButton.innerHTML = textWithClipboard;
+      }, 1500);
+    } catch (error) {
+      console.error("Could not copy text: ", error);
     }
-  );
-});
+  });
 
-const attachButton = document.querySelector('button[aria-label="Attach"]');
-
-// 👇️ Get computed styles of original element
-const attachButtonStyles = window.getComputedStyle(attachButton);
-
-let cssText = attachButtonStyles.cssText;
-
-if (!cssText) {
-  cssText = Array.from(attachButtonStyles).reduce((str, property) => {
-    return `${str}${property}:${attachButtonStyles.getPropertyValue(
-      property
-    )};`;
-  }, "");
+  return newButton;
 }
 
-// 👇️ Assign CSS styles to the element
-copyTicketButton.style.cssText = cssText;
+function getCssText(element) {
+  const styles = window.getComputedStyle(element);
+  let cssText = styles.cssText;
 
-copyTicketButton.style.width = "auto";
+  if (!cssText) {
+    cssText = Array.from(styles).reduce((str, property) => {
+      return `${str}${property}:${styles.getPropertyValue(property)};`;
+    }, "");
+  }
+
+  return cssText;
+}
+
+function addCopyButtons(issueHeader, issueIdSpan) {
+  if (!issueHeader || !issueIdSpan) {
+    return;
+  }
+
+  // get the issue ID and Title
+  const issueIdText = issueIdSpan.textContent;
+  const issueTitleText = document.querySelector(
+    'h1[data-test-id="issue.views.issue-base.foundation.summary.heading"]'
+  ).textContent;
+
+  // copy existing button styles to use for new buttons
+  const attachButton = document.querySelector('button[aria-label="Attach"]');
+  const existingButtonCssText = getCssText(attachButton);
+
+  // create and insert container for our new copy buttons
+  const newButtonsContainer = document.createElement("div");
+  newButtonsContainer.style.display = "flex";
+  newButtonsContainer.style.columnGap = "8px";
+  newButtonsContainer.setAttribute("id", "copyButtons");
+
+  const actionsContainer = issueHeader.firstChild.firstChild.firstChild;
+  actionsContainer.insertAdjacentElement("afterEnd", newButtonsContainer);
+  actionsContainer.style.flexGrow = "2";
+
+  // create and add copy buttons
+  const newButtonsProperties = [
+    {
+      buttonText: "ID",
+      copyText: issueIdText,
+      width: "52px",
+    },
+    {
+      buttonText: "Title",
+      copyText: issueTitleText,
+      width: "67px",
+    },
+    {
+      buttonText: "Title (ID)",
+      copyText: `${issueTitleText} (${issueIdText})`,
+      width: "97px",
+    },
+  ];
+
+  newButtonsProperties.forEach((buttonProperties) => {
+    const newButton = createCopyButton(buttonProperties, existingButtonCssText);
+
+    newButtonsContainer.appendChild(newButton);
+  });
+}
+
+// issues may not be present when page loads and can be viewed dynamically (ex: in modals opened by user)
+// watch for DOM changes so we know when to attempt to add copy buttons
+const observer = new MutationObserver((_mutationList) => {
+  const issueHeader = document.getElementById("jira-issue-header");
+  const issueIdSpan = document.querySelector(
+    'a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"] > span'
+  );
+
+  const areRequiredElementsPresent = issueHeader && issueIdSpan;
+  const areCopyButtonsPresent = document.getElementById("copyButtons");
+
+  if (!areCopyButtonsPresent && areRequiredElementsPresent) {
+    addCopyButtons(issueHeader, issueIdSpan);
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
